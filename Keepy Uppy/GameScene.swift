4dbf6b82de419b9e596e5a647a0343b174db4fcb
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-09-03 09:58:28 -0700

    Added files

---
+++ Keepy Uppy/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Keepy Uppy
+//
+//  Created by Matthew Allen Lin on 9/3/15.
+//  Copyright (c) 2015 Matthew Allen Lin. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ Keepy UppyTests/Keepy_UppyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Keepy_UppyTests.swift
+//  Keepy UppyTests
+//
+//  Created by Matthew Allen Lin on 9/3/15.
+//  Copyright (c) 2015 Matthew Allen Lin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Keepy_UppyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
