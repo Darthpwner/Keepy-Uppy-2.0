35270189e6c366bb5efd257f31aede90174094b9
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-09-03 20:37:15 -0700

    Ball spins around :)

--- Keepy Uppy/GameScene.swift
+++ Keepy Uppy/GameScene.swift
@@ -33,9 +33,39 @@ class GameScene: SKScene {
         runAction(SKAction.playSoundFileNamed(sound, waitForCompletion: false))
     }
     
-    override func didMoveToView(view: SKView) {        
-        ball.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
-        addChild(ball)
+    override func didMoveToView(view: SKView) {
+        // we live in a world with gravity on the y axis
+        self.physicsWorld.gravity = CGVectorMake(0, -6)
+        // we put contraints on the top, left, right, bottom so that our balls can bounce off them
+        let physicsBody = SKPhysicsBody (edgeLoopFromRect: self.frame)
+        // we set the body defining the physics to our scene
+        self.physicsBody = physicsBody
+        
+        // SkShapeNode is a primitive for drawing like with the AS3 Drawing API
+        // it has built in support for primitives like a circle, so we pass a radius
+        let shape = SKShapeNode(circleOfRadius: 20)
+        // we set the color and line style
+        shape.strokeColor = UIColor(red: 255, green: 0, blue: 0, alpha: 0.5)
+        shape.lineWidth = 4
+        
+        // we add each circle to the display list
+        self.addChild(shape)
+        
+        // this is the most important line, we define the body
+        shape.physicsBody = SKPhysicsBody(circleOfRadius: shape.frame.size.width/2)
+        // this defines the mass, roughness and bounciness
+        shape.physicsBody!.friction = 0.3
+        shape.physicsBody!.restitution = 0.8
+        shape.physicsBody!.mass = 0.5
+        // this will allow the balls to rotate when bouncing off each other
+        shape.physicsBody!.allowsRotation = true
+
+        
+        ball.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)  //Set the ball's position
+        addChild(ball)  //Add ball to the display list
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: shape.frame.size.width/2)
+        
     }
     
     override func update(currentTime: CFTimeInterval) {
