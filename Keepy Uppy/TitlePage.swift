5010a1c72fa326d39007022a779c83ae0d953e88
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-09-06 22:27:18 -0700

    Black fucking magic, the animated GIF scales bitches

--- Keepy Uppy/TitlePage.swift
+++ Keepy Uppy/TitlePage.swift
@@ -39,7 +39,9 @@ class TitlePage: UIViewController {
             .URLForResource("Bouncing Ball", withExtension: "gif")!)
         let bouncingBall = UIImage.animatedImageWithData(imageData!)
         var animation = UIImageView(image: bouncingBall)
-        //view.addSubview(animation)
+        animation.frame = CGRect(x: 0.0, y: 200.0, width: 400.0, height: 200.0)
+        
+        view.addSubview(animation)
         
         //Plays start song
         singleton.prepareAudios()
