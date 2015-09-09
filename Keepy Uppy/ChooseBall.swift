8894092c423edef0a1dfba53287b9bcc90694386
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-09-08 21:44:12 -0700

    Created singleton for Choose Ball

--- Keepy Uppy/ChooseBall.swift
+++ Keepy Uppy/ChooseBall.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ChooseBall: UIViewController {
     
+    var ballType: BallType?
+    
+    class var sharedInstance: ChooseBall {
+        struct Static {
+            static var onceToken: dispatch_once_t = 0
+            static var instance: ChooseBall? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.instance = ChooseBall()
+        }
+        return Static.instance!
+    }
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
