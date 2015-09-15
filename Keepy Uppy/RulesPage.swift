0cb7bd1878ace548fe0fe7fba71505e916a22b23
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro-2.local>
2015-09-15 01:20:13 -0700

    Fixed console output messages that were trivial

---
+++ Keepy Uppy/RulesPage.swift
@@ -0,0 +1,12 @@
+//
+//  RulesPage.swift
+//  Keepy Uppy
+//
+//  Created by Matthew Allen Lin on 9/15/15.
+//  Copyright (c) 2015 Matthew Allen Lin. All rights reserved.
+//
+
+import UIKit
+
+//Created just to avoid Console Output messages
+class RulesPage: UIViewController {}
